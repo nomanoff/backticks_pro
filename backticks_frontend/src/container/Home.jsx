2e6f7600f630c33417668d001893c848d96199be
Azizbek Nomanoff <im.nomanoff@gmail.com>
2022-01-15 13:38:23 +0900

    new components added / designing home ;

--- backticks_frontend/src/container/Home.jsx
+++ backticks_frontend/src/container/Home.jsx
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import {} from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+import { Sidebar, UserProfile } from "../components";
+import Pins from "./Pins";
+import { client } from "../client";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return <div>Home</div>;
