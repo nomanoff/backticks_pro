067552516a52cc273e3441cc4d3ce54aeefc3b6f
Azizbek Nomanoff <im.nomanoff@gmail.com>
2022-01-15 18:37:48 +0900

    fetching for search query implemented ✅ done for today, I think 😅

--- backticks_frontend/src/components/Feed.jsx
+++ backticks_frontend/src/components/Feed.jsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { client } from "../client";
+import { searchQuery } from "../utils/data";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+
+        setLoading(false);
+      });
+    } else {
+    }
+  }, [categoryId]);
+
   if (loading) {
     return <Spinner message="Loading..." />;
   }
+
   return <div>Feed</div>;
 };
 
