156526f0f7fe420ab0daf5797e409d410d2a3339
Azizbek Nomanoff <im.nomanoff@gmail.com>
2022-01-15 17:18:42 +0900

    home, sidebar and navbar is done ✅

--- backticks_frontend/src/container/Pins.jsx
+++ backticks_frontend/src/container/Pins.jsx
@@ -9,7 +9,11 @@ const Pins = ({ user }) => {
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
-        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Navbar
+          user={user}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
       </div>
       <div className="h-full">
         <Routes>
