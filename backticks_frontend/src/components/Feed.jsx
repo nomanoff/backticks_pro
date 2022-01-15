e7ca3799a72c978e5c9995fe045cff7377a2fe6a
Azizbek Nomanoff <im.nomanoff@gmail.com>
2022-01-15 18:20:30 +0900

    custom spinner component is added ✅"

--- backticks_frontend/src/components/Feed.jsx
+++ backticks_frontend/src/components/Feed.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { client } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  if (loading) {
+    <Spinner message="Loading..." />;
+  }
   return <div>Feed</div>;
 };
 
