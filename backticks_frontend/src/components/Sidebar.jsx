7f6c6ad504430ad8a1fd8c3d91050842b3ea20e1
Azizbek Nomanoff <im.nomanoff@gmail.com>
2022-04-16 13:47:45 +0900

    Sidebar is done && show pictures of categories ✅

--- backticks_frontend/src/components/Sidebar.jsx
+++ backticks_frontend/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
 
+import { categories } from "../utils/data";
 import logo from "../assets/logo.png";
 
 const isNotActiveStyle =
@@ -9,15 +10,6 @@ const isNotActiveStyle =
 
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
-
-const categories = [
-  { name: "Animals" },
-  { name: "Flowers" },
-  { name: "Cities" },
-  { name: "Coding" },
-  { name: "Cats" },
-  { name: "Other" },
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -60,6 +52,11 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                className=" w-8 h-8 rounded-full shadow-sm"
+                alt="category"
+              />
               {category.name}
             </NavLink>
           ))}
